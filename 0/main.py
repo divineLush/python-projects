a2b673c7eb7e8cbdf1c09cae6589694df2d50512
divineLush <alvipath@yahoo.com>
2021-09-08 22:57:37 +0700

    0

---
+++ 0/main.py
@@ -0,0 +1,6 @@
+print('welcome to the band name generator\n')
+
+city = input('what\'s the name of the city you grew up in?\n')
+pet = input('what\'s your pet\'s name?\n')
+
+print('your band name could be ' + city + ' ' + pet)
